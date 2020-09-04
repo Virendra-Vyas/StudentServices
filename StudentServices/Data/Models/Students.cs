50c8635b903869485abdf774be272495d6074bef
virendra-vyas <virendra1629@gmail.com>
2020-09-04 16:57:08 +0100

    Add project files.

---
+++ StudentServices/Data/Models/Students.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentServices.Data.Models
+{
+    public class Students
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        [Required]
+        [MaxLength(3)]
+        public int Age { get; set; }
+    }
+}
---
+++ StudentServices/Migrations/20200904104157_update.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentServices.Migrations
+{
+    public partial class update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "students",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "students",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
